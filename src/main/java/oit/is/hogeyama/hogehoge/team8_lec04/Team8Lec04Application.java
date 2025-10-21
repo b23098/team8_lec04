7a5c4c6b73bfeb7552ea27d88363a21ba9bb7c4e
b23098 <e1b23098@oit.ac.jp>
2025-10-21 13:56:40 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/hogeyama/hogehoge/team8_lec04/Team8Lec04Application.java
@@ -0,0 +1,13 @@
+package oit.is.hogeyama.hogehoge.team8_lec04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team8Lec04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team8Lec04Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/hogeyama/hogehoge/team8_lec04/Team8Lec04ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.hogeyama.hogehoge.team8_lec04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team8Lec04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
